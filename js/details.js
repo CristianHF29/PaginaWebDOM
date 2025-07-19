6797a42142a67532c82803d2bf84b885d2f223d2
CristianHF29 <cristart.hernandezf@gmail.com>
2025-07-19 10:00:44 -0600

    :tada: Starting project design with pages, assets, and js functions still waiting for secondary functions on  billboard actions

---
+++ js/details.js
@@ -0,0 +1,16 @@
+
+/* Codigo para desactivar y activar sonido del video*/
+
+const video = document.getElementById('videoTrailer');
+const btn = document.getElementById('toggleSoundBtn');
+const icon = btn.querySelector('i');
+
+btn.addEventListener('click', () => {
+    video.muted = !video.muted;
+    icon.className = video.muted ? "bi bi-volume-mute-fill" : "bi bi-volume-up-fill";
+    video.play();
+});
+
+
+/* Codigo para mostrar imagen en detalles dependiendo del id*/ 
+
